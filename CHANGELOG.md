30c461c1705c0f24d89d73e95c5fc8babc9e371c
Onnimanni Hannonen <onnari11@gmail.com>
2023-04-14 14:25:04 +0300

    feat: publishes v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
-# 0.0.1
+# 0.0.2
+- Exports package:logger
 
+# 0.0.1
 - The initial release
--- lib/mankeli_core.dart
+++ lib/mankeli_core.dart
@@ -1,4 +1,6 @@
 library mankeli_core;
 
+export 'package:logger/logger.dart';
+
 export 'src/app_logger.dart';
 export 'src/repository.dart';
